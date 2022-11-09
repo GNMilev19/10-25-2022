8f5a59bd15accca12c2a5842434ce927851050f2
Георги Николаев Милев <GNMilev19@codingburgas.bg>
2022-11-09 09:24:18 +0200

    11/09/2022

--- MessyClassroom/MessyClassroom/MessyClassroom.cpp
+++ MessyClassroom/MessyClassroom/MessyClassroom.cpp
@@ -1,49 +1,94 @@
 #include <iostream>
-#include<thread>
-#include<string>
-#include<iomanip>
-#include<mutex>
+#include <iomanip>
+#include <thread>
+#include <string>
+#include <mutex>
+#include <random>
+#include <conio.h>
+
+
 
 using namespace std;
 
+
+
+
+
 string msgs[]{
-    "Blabla", "Azis", "Galena",
-    "Fiki", "Preslava", "Toni", "Storaro"
+    "Kude si", "Off", "Da tochno","Beshe qsno"
+    "eiii", "vurni tela", "molqta"
 };
+
+
+
+
 
 void log(string msg)
 {
     auto tid = this_thread::get_id();
-    cout << tid << setw(5) << " | " << msg << endl;
+    cout << tid << setw(5) << " | " << msg << "\n";
 }
+
+
 
 mutex mu;
 
-int getRandNumber(int upperLimit)
-{
-    lock_guard<mutex> lock(mu);
 
-    return rand() % upperLimit;
+
+
+int getRandomNumber(const int& min, const int& max) {
+    static thread_local std::random_device rd;
+    static thread_local std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> distribution(min, max);
+    return distribution(mt);
 }
+
+
+
 
 void sleepRandMillis()
 {
-    int millis = getRandNumber(1000) + 800;
+    int millis = getRandomNumber(800, 1000);
+
+
 
     this_thread::sleep_for(chrono::milliseconds(millis));
 }
 
-void studentThreadFunc() 
+
+
+bool doExit = false;
+
+
+
+void studentThreadFunc()
 {
     rand();
-    while (true) 
-    {
-        int index = getRandNumber(7);
 
-        cout << msgs[index] << endl;
+
+
+    while (true)
+    {
+        int index = getRandomNumber(0, 6);
+
+
+
+        cout << msgs[index] << "\n";
+
+
+
+        if (doExit == true) {
+            log("Exitting....");
+            break;
+        }
+
+
+
         sleepRandMillis();
     }
 }
+
+
 
 int main()
 {
@@ -51,7 +96,37 @@ int main()
     sleepRandMillis();
     thread student2(studentThreadFunc);
     sleepRandMillis();
+    thread student3(studentThreadFunc);
+    sleepRandMillis();
+
+
+
+    while (true) {
+        auto keyCode = _getch();
+
+
+
+        if (keyCode == 27) {
+            doExit = true;
+            break;
+        }
+        if (keyCode == 32) {
+
+
+        }
+    }
+
+
+
+    auto keyCode = _getch();
+
+
+
+    cout << keyCode;
+
+
 
     student1.join();
     student2.join();
+    student3.join();
 }
